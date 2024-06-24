1792e815388578c7d78b2fc1bc3ec1e2c066e469
Pablo Escobar <pablo-9906@outlook.es>
2024-06-23 22:06:26 -0500

    feat: crud users

--- routes/api.php
+++ routes/api.php
@@ -3,8 +3,26 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\api\CharactersController;
 use App\Http\Controllers\api\EpisodesController;
+use App\Http\Controllers\api\UsersController;
+
+Route::get('/health', function () {
+    return response()->json([
+        'message' => 'Welcome to the Rick and Morty API',
+        'status' => 200
+    ], 200);
+});
+
+Route::post('/login', [UsersController::class, 'login']);
 
 Route::group(['prefix' => 'v1'], function () {
+    Route::group(['prefix' => 'users'], function () {
+        Route::get('/', [UsersController::class, 'index']);
+        Route::get('/{id}', [UsersController::class, 'show']);
+        Route::post('/', [UsersController::class, 'store']);
+        Route::put('/{id}', [UsersController::class, 'update']);
+        Route::patch('/{id}', [UsersController::class, 'updatePartial']);
+        Route::delete('/{id}', [UsersController::class, 'destroy']);
+    });
     Route::group(['prefix' => 'episodes'], function () {
         Route::get('/', [EpisodesController::class, 'index']);
         Route::get('/{id}', [EpisodesController::class, 'show']);
